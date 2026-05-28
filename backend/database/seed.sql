c405c4a87695bff22ef197416899ae78828c21f8
Allsz <allsz@outlook.com>
2026-05-28 20:01:44 -0300

    Quick Fix

--- backend/database/seed.sql
+++ backend/database/seed.sql
@@ -5,7 +5,7 @@ DELETE FROM PRODUTOS;
 DELETE FROM CATEGORIAS;
 DELETE FROM USUARIOS;
 
--- Colocando o usuário admin para teste
+
 INSERT INTO USUARIOS (NOME, EMAIL, SENHA, TIPO) VALUES
 ('Administrador', 'admin@vitrinevirtual.com', '$2b$10$rP5n8b9VexF1Y2HmWgBvJuQ7QG8yWzB2z/c0R.QG5u0CgW0p8N5Jq', 'admin'); -- Senha: admin123
 
@@ -30,7 +30,9 @@ INSERT INTO PRODUTOS (NOME, DESCRICAO, PRECO, IMAGEM_URL, ESTOQUE, MADEIRA, PESO
 ('Taco Masterpiece Ébano', 'A obra de arte da nossa vitrine. Base em Ébano imperial com marchetaria artesanal em Jacarandá.', 1250.00, 'https://images.unsplash.com/photo-1609137144813-90d1bf3b867c?q=80&w=400', 2, 'Ébano Imperial & Jacarandá', '580g', '1.50m', 'Metal 10mm', 3),
 ('Taco Champion Gold Edition', 'Edição especial assinada por campeões. Seleção ultra rigorosa de madeira e balanceamento milimétrico.', 1500.00, 'https://images.unsplash.com/photo-1609137144813-90d1bf3b867c?q=80&w=400', 1, 'Goiabão Especial', '565g', '1.48m', 'Celerom 10mm', 3);
 
--- 4. Colocando uma mensagem de exemplo para teste
+-- 4. Mudar essa tabela (Mudou a finalizade no front)
 INSERT INTO ORCAMENTOS (NOME, EMAIL, WHATSAPP, MENSAGEM, PRODUTO_DESEJADO_ID, STATUS) VALUES
 ('Carlos Silva', 'carlos@gmail.com', '11999998888', 'Gostaria de saber se o Taco Masterpiece Ébano tem frete grátis para São Paulo.', 5, 'NOVO'),
 ('Mariana Costa', 'mariana.costa@hotmail.com', '21988887777', 'Quero encomendar um taco personalizado com peso de 590g e madeira Goiabão.', NULL, 'EM_ATENDIMENTO');
+
+SELECT * FROM USUARIOS;
